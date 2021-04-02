616859ca614978bccf61c1400a201ac05d000759
nishanth <gayatridipali@gmail.com>
2021-04-02 10:32:03 +0530

    done with bye

---
+++ api/bye.ts
@@ -0,0 +1,33 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import sgMail from "@sendgrid/mail";
+import "dotenv/config";
+
+type input = {
+  to: string;
+  username: string;
+};
+
+const sendByeEmail = async (req: VercelRequest, res: VercelResponse) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+  const { to, username }: input = req.body;
+
+  const msg = {
+    to, // Change to your recipient
+    from: "nishanthdipali@gmail.com", // Change to your verified sender
+    templateId: process.env.SENDGRID_BYE_TEMPLATE_ID!,
+    dynamic_template_data: {
+      username,
+    },
+  };
+
+  try {
+    await sgMail.send(msg);
+    res.send("email sent");
+    console.log("Email sent");
+  } catch (error) {
+    res.status(500).json({ error });
+    console.error(error);
+  }
+};
+
+export default sendByeEmail;
